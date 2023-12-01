120bacd9b06ea7a688126d371642e7529dc48fbb
Matt <matt@invevo.com>
2023-12-01 12:29:43 +0000

    Day 1 Part 2

--- Day 1/src/main/kotlin/Main.kt
+++ Day 1/src/main/kotlin/Main.kt
@@ -1,15 +1,59 @@
 fun main() {
-    val calibrations = input
-        .toCalibrations()
+    part1()
+    part2()
+}
 
-    val result = calibrations.sum()
+private fun part1() {
+    val result = input
+        .toCalibrations(includeWords = false)
+        .sum()
 
     println("Part 1 | Answer: $result")
 }
 
-private fun List<String>.toCalibrations(): List<Int> =
-    this.map { amendedCalibration ->
-        val firstDigit = amendedCalibration.first { it.isDigit() }
-        val lastDigit = amendedCalibration.lastOrNull { it.isDigit() } ?: firstDigit
-        "$firstDigit$lastDigit".toInt()
+private fun part2() {
+    val result = input
+        .toCalibrations(includeWords = true)
+        .sum()
+
+    println("Part 2 | Answer: $result")
+}
+
+private fun List<String>.toCalibrations(includeWords: Boolean = false): List<Int> =
+    this.map { it.toCalibration(includeWords) }
+
+private fun String.toCalibration(includeWords: Boolean): Int {
+    val digitsFromInput = this.extractNumbers(includeWords)
+    return "${digitsFromInput.first()}${digitsFromInput.last()}".toInt()
+}
+
+private fun String.extractNumbers(includeWords: Boolean): List<Int> {
+    val validDigits = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+    val validWords = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    val valuesToExtract = if (includeWords) {
+        validWords.plus(validDigits)
+    } else {
+        validDigits
+    }
+
+    val regex = """(?=(${valuesToExtract.joinToString(separator = "|")}))""".toRegex()
+    return regex.findAll(this)
+        .map { it.groupValues.last() }
+        .map { it.toNumber() }
+        .toList()
+}
+
+private fun String.toNumber(): Int =
+    when (this) {
+        "1", "one" -> 1
+        "2", "two" -> 2
+        "3", "three" -> 3
+        "4", "four" -> 4
+        "5", "five" -> 5
+        "6", "six" -> 6
+        "7", "seven" -> 7
+        "8", "eight" -> 8
+        "9", "nine" -> 9
+        else -> this.toInt()
     }

19cff0fe0ded979dc5c93a571a66605030a5eb3e
Matt <matt@invevo.com>
2023-12-03 12:05:23 +0000

    Day 3 Part 1

---
+++ Day 3/src/main/kotlin/Main.kt
@@ -0,0 +1,116 @@
+fun main() {
+    part1()
+}
+
+private fun part1() {
+    val result = input
+        .toSchematic()
+        .getPartNumbers()
+        .sum()
+
+    println("Part 1 | Answer: $result")
+}
+
+private fun List<String>.toSchematic(): Map<Coordinate, SchematicPart> =
+    this.toSchematicParts().associateBy {
+        it.coordinate
+    }
+
+private fun List<String>.toSchematicParts(): List<SchematicPart> =
+    this.flatMapIndexed { y, value ->
+        val parts = mutableListOf<SchematicPart>()
+        var currentPart = ""
+        var currentX = 0
+        value.forEach { partChar ->
+            when {
+                partChar.isDigit() -> {
+                    currentPart = if(currentPart.toIntOrNull() != null || currentPart.isEmpty()) {
+                        currentPart + partChar
+                    } else {
+                        val type = if (currentPart == ".") {
+                            SchematicPartType.NOTHING
+                        } else {
+                            SchematicPartType.SYMBOL
+                        }
+                        val part = SchematicPart(Coordinate(x = currentX- currentPart.length, y = y), currentPart, type)
+                        parts.add(part)
+
+                        "".plus(partChar)
+                    }
+                }
+                else -> {
+                    currentPart = if(currentPart.toIntOrNull() != null) {
+                        val part = SchematicPart(Coordinate(x = currentX - currentPart.length, y = y), currentPart, SchematicPartType.NUMBER)
+                        parts.add(part)
+                        "".plus(partChar)
+                    } else {
+                        val type = if (currentPart == ".") {
+                            SchematicPartType.NOTHING
+                        } else {
+                            SchematicPartType.SYMBOL
+                        }
+
+                        if(currentPart.isNotEmpty()) {
+                            val part =
+                                SchematicPart(Coordinate(x = currentX - currentPart.length, y = y), currentPart, type)
+                            parts.add(part)
+                        }
+
+                        "".plus(partChar)
+                    }
+                }
+            }
+            currentX++
+        }
+
+        when {
+            currentPart.toIntOrNull() != null -> {
+                val part = SchematicPart(Coordinate(x = currentX - currentPart.length, y = y), currentPart, SchematicPartType.NUMBER)
+                parts.add(part)
+            }
+            currentPart == "." -> {
+                val part = SchematicPart(Coordinate(x = currentX - currentPart.length, y = y), currentPart, SchematicPartType.NOTHING)
+                parts.add(part)
+            }
+            else -> {
+                val part = SchematicPart(Coordinate(x = currentX - currentPart.length, y = y), currentPart, SchematicPartType.SYMBOL)
+                parts.add(part)
+            }
+        }
+        parts
+    }
+
+private fun Map<Coordinate, SchematicPart>.getPartNumbers(): List<Int> =
+    this.entries
+        .filter { it.value.type == SchematicPartType.NUMBER }
+        .filter { schematicPart ->
+            var coordinateStart = schematicPart.key
+            val coordinateEnd = Coordinate(x = coordinateStart.x + schematicPart.value.content.length - 1, y = coordinateStart.y)
+            val surroundingParts = this.getAdjacentParts(coordinateStart, coordinateEnd)
+            surroundingParts.any { it.type == SchematicPartType.SYMBOL }
+        }.map {
+            it.value.content.toInt()
+        }
+
+private fun Map<Coordinate, SchematicPart>.getAdjacentParts(startCoordinate: Coordinate, endCoordinate: Coordinate): List<SchematicPart> {
+    val xRange = startCoordinate.x - 1..endCoordinate.x + 1
+    val yRange = startCoordinate.y -1..endCoordinate.y + 1
+    return xRange.flatMap { x ->
+        yRange.map { y ->
+            if(x >= startCoordinate.x && x <= endCoordinate.x && y == startCoordinate.y) {
+                null
+            } else {
+                val adjacentPartCoordinate = Coordinate(x, y)
+                this[adjacentPartCoordinate]
+            }
+        }
+    }.filterNotNull()
+}
+
+data class SchematicPart(val coordinate: Coordinate, val content: String, val type: SchematicPartType)
+data class Coordinate(val x: Int, val y: Int)
+enum class SchematicPartType {
+    NOTHING,
+    NUMBER,
+    SYMBOL
+}

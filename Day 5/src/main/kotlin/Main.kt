659eb9e02b9d60d1e7a68b78f0faeb915b17db94
Matt <matt@invevo.com>
2023-12-05 12:45:15 +0000

    Day 5 Part 2

--- Day 5/src/main/kotlin/Main.kt
+++ Day 5/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
     part1()
+    part2()
 }
 
 private fun part1() {
@@ -25,6 +26,31 @@ private fun part1() {
     println("Part 1 | Answer: $result")
 }
 
+private fun part2() {
+    val seedRanges = input.toSeedRanges()
+    val seedToSoilMap = input.toSeedMap("seed", "soil")
+    val soilToFertilizerMap = input.toSeedMap("soil", "fertilizer")
+    val fertilizerToWaterMap = input.toSeedMap("fertilizer", "water")
+    val waterToLightMap = input.toSeedMap("water", "light")
+    val lightToTemperatureMap = input.toSeedMap("light", "temperature")
+    val temperatureToHumidityMap = input.toSeedMap("temperature", "humidity")
+    val humidityToLocationMap = input.toSeedMap("humidity", "location")
+
+    val result = seedRanges.minOf { seedRange ->
+        seedRange.toMinLocation(
+            seedToSoilMap,
+            soilToFertilizerMap,
+            fertilizerToWaterMap,
+            waterToLightMap,
+            lightToTemperatureMap,
+            temperatureToHumidityMap,
+            humidityToLocationMap
+        )
+    }
+
+    println("Part 2 | Answer: $result")
+}
+
 private fun List<String>.toSeeds(): List<Long> =
     this.first {
         it.startsWith("seeds:")
@@ -32,6 +58,20 @@ private fun List<String>.toSeeds(): List<Long> =
         .split(":")[1].trim()
         .split(" ")
         .map { it.toLong() }
+
+private fun List<String>.toSeedRanges(): List<LongRange> {
+    val ranges = this.first {
+        it.startsWith("seeds:")
+    }
+        .split(":")[1].trim()
+        .split(" ")
+        .map { it.toLong() }
+        .chunked(2)
+
+    return ranges.map { (startSeed, rangeLength) ->
+        (startSeed .. startSeed + rangeLength)
+    }
+}
 
 private fun List<String>.toSeedMap(source: String, destination: String): List<SeedMap> {
     val startOfMap = this.dropWhile { it != "$source-to-$destination map:" }
@@ -52,8 +92,17 @@ private fun List<Long>.toLocations(vararg seedMaps: List<SeedMap>): List<Long> =
         input
     }
 
+private fun LongRange.toMinLocation(vararg seedMaps: List<SeedMap>): Long =
+    this.minOf { seed ->
+        var input = seed
+        seedMaps.map {
+            input = input.mapTo(it)
+        }
+        input
+    }
+
 private fun Long.mapTo(seedMap: List<SeedMap>): Long {
-    val matchingMap = seedMap.firstOrNull { (it.sourceRangeStart .. (it.sourceRangeStart + it.rangeLength)).contains(this) }
+    val matchingMap = seedMap.firstOrNull { (it.sourceRangeStart until (it.sourceRangeStart + it.rangeLength)).contains(this) }
     return if(matchingMap != null) {
         matchingMap.destRangeStart + (this - matchingMap.sourceRangeStart)
     } else {

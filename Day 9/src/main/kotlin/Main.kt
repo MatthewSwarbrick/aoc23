f60ea0997a33a764a7df8744520c6d988626d341
Matt <matt@invevo.com>
2023-12-09 09:29:23 +0000

    Day 9 Part 2

--- Day 9/src/main/kotlin/Main.kt
+++ Day 9/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
     part1()
+    part2()
 }
 
 private fun part1() {
@@ -10,12 +11,20 @@ private fun part1() {
     println("Part 1 | Answer: $result")
 }
 
-private fun List<String>.toSequences(): List<Sequence> =
+private fun part2() {
+    val result = input
+        .toSequences(isExtrapolateBack = true)
+        .sumOf { it.numbers.first() }
+
+    println("Part 1 | Answer: $result")
+}
+
+private fun List<String>.toSequences(isExtrapolateBack: Boolean = false): List<Sequence> =
     this.map { input ->
         input
             .toSequence()
-            .extrapolate()
-            .toNewSequence()[0]
+            .let { if(isExtrapolateBack) { it.extrapolateBack() } else { it.extrapolate()} }
+            .toNewSequence(isExtrapolateBack)[0]
     }
 
 private fun String.toSequence(): Sequence =
@@ -44,22 +53,67 @@ private fun Sequence.extrapolate(): List<Sequence> {
     return sequences
 }
 
-private fun List<Sequence>.toNewSequence(): List<Sequence> {
+private fun Sequence.extrapolateBack(): List<Sequence> {
+    val sequences = mutableListOf(this)
+
+    var sequenceToExtrapolate = this.numbers
+    while(!sequenceToExtrapolate.all { it == 0 }) {
+        sequenceToExtrapolate = sequenceToExtrapolate
+            .mapIndexedNotNull { index, value ->
+                if(index == 0) {
+                    null
+                } else {
+                    value - sequenceToExtrapolate[index - 1]
+                }
+            }
+
+        val sequence = Sequence(sequenceToExtrapolate)
+        sequences.add(sequence)
+    }
+
+    return sequences
+}
+
+private fun List<Sequence>.toNewSequence(isExtrapolateBack: Boolean): List<Sequence> {
     if(this.count() == 1) return this
 
-    val lastSequenceValue = this.getLastValue()
+    val previousSequenceValue = if(isExtrapolateBack) {
+        this.getFirstValue()
+    } else {
+        this.getLastValue()
+    }
+
     val filteredSequences = this.take(this.count() - 1)
-    val newLastSequenceValue = lastSequenceValue + filteredSequences.getLastValue()
+    val currentSequenceValue = if(isExtrapolateBack) {
+        filteredSequences.getFirstValue()
+    } else {
+        filteredSequences.getLastValue()
+    }
+
+    val newValue = if(isExtrapolateBack) {
+        currentSequenceValue - previousSequenceValue
+    } else {
+        previousSequenceValue + currentSequenceValue
+    }
+    
     return filteredSequences.mapIndexed { index, sequence ->
         if(index != filteredSequences.count() - 1) {
             sequence
         } else {
-            Sequence(sequence.numbers.plus(newLastSequenceValue))
+            if(isExtrapolateBack) {
+                val newNumbers = listOf(newValue).plus(sequence.numbers)
+                Sequence(newNumbers)
+            } else {
+                Sequence(sequence.numbers.plus(newValue))
+            }
         }
-    }.toNewSequence()
+    }.toNewSequence(isExtrapolateBack)
 }
 
 private fun List<Sequence>.getLastValue(): Int =
     this.last().numbers.last()
+
+private fun List<Sequence>.getFirstValue(): Int =
+    this.last().numbers.first()
 
 data class Sequence(val numbers: List<Int>)

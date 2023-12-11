77a0e4108cfdcb1cc26a095659c3d1ddf636ab7e
Matt <matt@invevo.com>
2023-12-11 23:08:28 +0000

    Day 11 Part 2

--- Day 11/src/main/kotlin/Main.kt
+++ Day 11/src/main/kotlin/Main.kt
@@ -2,11 +2,12 @@ import kotlin.math.abs
 
 fun main() {
     part1()
+    part2()
 }
 
 private fun part1() {
     val result = input
-        .toSpaceMap().apply { print() }.apply { println() }
+        .toSpaceMap()
         .expand().apply { print() }
         .toGalaxyPairs()
         .toGalaxyPairDistances()
@@ -15,10 +16,21 @@ private fun part1() {
     println("Part 1 | Answer: $result")
 }
 
+private fun part2() {
+    val result = input
+        .toSpaceMap()
+        .expand(999_999)
+        .toGalaxyPairs()
+        .toGalaxyPairDistances()
+        .sum()
+
+    println("Part 2 | Answer: $result")
+}
+
 private fun List<String>.toSpaceMap(): List<Space> =
     this.flatMapIndexed { y, row ->
         row.mapIndexed { x, spaceChar ->
-            val coordinate = Coordinate(x, y)
+            val coordinate = Coordinate(x.toLong(), y.toLong())
             val spaceType = if (spaceChar == '.') {
                 SpaceType.EMPTY
             } else {
@@ -28,52 +40,52 @@ private fun List<String>.toSpaceMap(): List<Space> =
         }
     }
 
-private fun List<Space>.expand(): List<Space> {
+private fun List<Space>.expand(expandSize: Int = 1): List<Space> {
     val columnsToExpand = this.groupBy { it.coordinate.x }
         .filter { column -> column.value.all { it.type == SpaceType.EMPTY } }
         .map { it.key }.sorted()
 
-    var expandedSpace = this
-    columnsToExpand.forEach { columnToExpand ->
-        val newColumn = expandedSpace.filter { it.coordinate.x == columnToExpand }
-        expandedSpace = expandedSpace.map {
-            if (it.coordinate.x <= columnToExpand) {
-                it.copy(coordinate = Coordinate(x = it.coordinate.x - 1, y = it.coordinate.y))
-            } else {
-                it
-            }
-        }.plus(newColumn)
-    }
-
-    val rowsToExpand = expandedSpace.groupBy { it.coordinate.y }
+    val rowsToExpand = this.groupBy { it.coordinate.y }
         .filter { column -> column.value.all { it.type == SpaceType.EMPTY } }
         .map { it.key }.sorted()
 
-    rowsToExpand.forEach { rowToExpand ->
-        val newColumn = expandedSpace.filter { it.coordinate.y == rowToExpand }
-        expandedSpace = expandedSpace.map {
-            if (it.coordinate.y <= rowToExpand) {
-                it.copy(coordinate = Coordinate(x = it.coordinate.x, y = it.coordinate.y - 1))
-            } else {
-                it
+    var expandedSpace = this.filter { it.type == SpaceType.GALAXY }
+    columnsToExpand.forEach { columnToExpand ->
+        repeat(expandSize) {
+            expandedSpace = expandedSpace.map {
+                if (it.coordinate.x <= columnToExpand) {
+                    it.copy(coordinate = Coordinate(x = it.coordinate.x - 1, y = it.coordinate.y))
+                } else {
+                    it
+                }
             }
-        }.plus(newColumn)
+        }
+    }
+
+    rowsToExpand.forEach { rowToExpand ->
+        repeat(expandSize) {
+            expandedSpace = expandedSpace.map {
+                if (it.coordinate.y <= rowToExpand) {
+                    it.copy(coordinate = Coordinate(x = it.coordinate.x, y = it.coordinate.y - 1))
+                } else {
+                    it
+                }
+            }
+        }
     }
 
     return expandedSpace
 }
 
-private fun List<Space>.toGalaxyPairs(): List<Pair<Space, Space>> {
-    val galaxySpaces = this.filter { it.type == SpaceType.GALAXY }
-    return galaxySpaces.flatMapIndexed { index, leftSpace ->
-        galaxySpaces.drop(index + 1)
+private fun List<Space>.toGalaxyPairs(): List<Pair<Space, Space>> =
+    this.flatMapIndexed { index, leftSpace ->
+        this.drop(index + 1)
             .map { rightSpace ->
                 leftSpace to rightSpace
             }
     }
-}
 
-private fun List<Pair<Space, Space>>.toGalaxyPairDistances(): List<Int> =
+private fun List<Pair<Space, Space>>.toGalaxyPairDistances(): List<Long> =
     this.map { (left, right) ->
         (abs(left.coordinate.x - right.coordinate.x) + abs(left.coordinate.y - right.coordinate.y))
     }
@@ -96,7 +108,7 @@ private fun List<Space>.print() {
 }
 
 data class Space(val coordinate: Coordinate, val type: SpaceType)
-data class Coordinate(val x: Int, val y: Int)
+data class Coordinate(val x: Long, val y: Long)
 
 enum class SpaceType {
     EMPTY,

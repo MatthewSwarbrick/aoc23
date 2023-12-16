e3da9baf071c4d505ffd966e26a2b9e4446a4287
Matt <matt@invevo.com>
2023-12-16 19:38:22 +0000

    Day 16 Part 2

--- Day 16/src/main/kotlin/Main.kt
+++ Day 16/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
     part1()
+    part2()
 }
 
 val visitedTiles: MutableSet<Pair<Coordinate, DirectionOfTravel>> = mutableSetOf()
@@ -14,6 +15,15 @@ private fun part1() {
         .count()
 
     println("Part 1 | Answer: $result")
+}
+
+private fun part2() {
+    visitedTiles.clear()
+    val result = input
+        .toTiles()
+        .toMaxEnergisedTiles()
+
+    println("Part 2 | Answer: $result")
 }
 
 private fun List<String>.toTiles(): Map<Coordinate, Char> =
@@ -39,6 +49,49 @@ private fun Map<Coordinate, Char>.toEnergisedTiles(
     newTileCoordinates.forEach {
         this.toEnergisedTiles(it.first, it.second)
     }
+}
+
+private fun Map<Coordinate, Char>.toMaxEnergisedTiles(): Int {
+    val maxY = this.maxOf { it.key.y }
+    val maxX = this.maxOf { it.key.x }
+    val topCoords = this.keys.filter { it.y == 0 }
+    val leftCoords = this.keys.filter { it.x == 0 }
+    val bottomCoords = this.keys.filter { it.y == maxY }
+    val rightCoords = this.keys.filter { it.x == maxX}
+
+    val maxFromTop = topCoords.maxOf { topCoord ->
+        visitedTiles.clear()
+        this.toEnergisedTiles(topCoord, DirectionOfTravel.DOWN)
+
+        visitedTiles
+            .map { it.first }.toSet().count()
+    }
+
+    val maxFromLeft = leftCoords.maxOf { leftCoord ->
+        visitedTiles.clear()
+        this.toEnergisedTiles(leftCoord, DirectionOfTravel.RIGHT)
+
+        visitedTiles
+            .map { it.first }.toSet().count()
+    }
+
+    val maxFromBottom = bottomCoords.maxOf { bottomCoord ->
+        visitedTiles.clear()
+        this.toEnergisedTiles(bottomCoord, DirectionOfTravel.UP)
+
+        visitedTiles
+            .map { it.first }.toSet().count()
+    }
+
+    val maxFromRight = rightCoords.maxOf { rightCoord ->
+        visitedTiles.clear()
+        this.toEnergisedTiles(rightCoord, DirectionOfTravel.LEFT)
+
+        visitedTiles
+            .map { it.first }.toSet().count()
+    }
+
+    return listOf(maxFromTop, maxFromLeft, maxFromBottom, maxFromRight).maxOf { it }
 }
 
 private fun toNewCoordinates(
